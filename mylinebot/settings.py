579492d03f03c75e39c38abf3c68ef12334fd2bb
shitinass <hsienson2@gmail.com>
2022-01-01 23:45:28 +0800

    make it better

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -127,4 +127,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')#加入static路徑
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+##STATIC_ROOT = os.path.join(BASE_DIR, 'static')
